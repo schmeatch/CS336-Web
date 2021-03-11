ce6e67e6ed9bfe6f6f2c3d6238489f9998d5c5ac
Aris Chung <archung000@gmail.com>
2021-03-10 22:03:51 -0500

    init pt2

--- cs336-Web/CS336-Web/WebContent/WEB-INF/registration.jsp
+++ cs336-Web/CS336-Web/WebContent/WEB-INF/registration.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
  <div align="center">
-  <h1>Employee Register Form</h1>
+  <h1>Registration Form</h1>
   <form action="<%= request.getContextPath() %>/register" method="post">
    <table style="with: 80%">
     <tr>
--- cs336-Web/CS336-Web/src/net/registration/controller/Servlet.java
+++ cs336-Web/CS336-Web/src/net/registration/controller/Servlet.java
@@ -1,11 +1,16 @@
 package net.registration.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.registration.dao.UserDAO;
+import net.registration.model.User;
 
 /**
  * Servlet implementation class Servlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/register")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private UserDAO userDAO = new UserDAO();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,6 +33,8 @@ public class Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatch = request.getRequestDispatcher("/WEB-INF/registration.jsp");
+        dispatch.forward(request, response);
 	}
 
 	/**
@@ -35,7 +42,31 @@ public class Servlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setContact(contact);
+        user.setAddress(address);
+
+        try {
+            userDAO.registerUser(user);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        RequestDispatcher dispatch = request.getRequestDispatcher("/WEB-INF/userdetails.jsp");
+        dispatch.forward(request, response);
+//        response.sendRedirect("userdetails.jsp");
+//		doGet(request, response);
 	}
 
 }
